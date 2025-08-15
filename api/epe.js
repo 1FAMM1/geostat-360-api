a3f3e61caed9227595a328144b22017101baa677
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2025-08-15 18:29:56 +0100

    Update epe.js

--- api/epe.js
+++ api/epe.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // Substitua pelos seus dados do Supabase
 const supabaseUrl = 'https://rjkbodfqsvckvnhjwmhg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqa2JvZGZxc3ZuaGp3bWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjM3NjQsImV4cCI6MjA2MzczOTc2NH0.jX5OPZkz1JSSwrahCoFzqGYw8tYkgE8isbn12uP43-0'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqa2JvZGZxc3Zja3ZuaGp3bWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjM3NjQsImV4cCI6MjA2MzczOTc2NH0.jX5OPZkz1JSSwrahCoFzqGYw8tYkgE8isbn12uP43-0'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default async function handler(req, res) {
