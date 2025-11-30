03ed6a71355f91e890007ca0efcb4bb7462b3550
GrupoCB360 <86024360+1FAMM1@users.noreply.github.com>
2025-11-30 12:36:57 +0000

    Update vehicle_control.js

--- api/vehicle_control.js
+++ api/vehicle_control.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
         allVehicles.push(vehicle.vehicle)
         vehicleStatuses[vehicle.vehicle] = vehicle.current_status || 'Disponível'
         vehicleINOP[vehicle.vehicle] = vehicle.is_inop
-        vehicleIDs[vehicle.vehicle] = vehicle.veic_id || null
+        vehicleIDs[vehicle.vehicle] = vehicle.vehic_id || null
       })
   
       return res.json({
